83b6be5db1dd5f8ecedf7b74f21d4733ca2e38d7
Prajesh Ananthan <prajesh.ananthan@outlook.com>
2016-12-26 01:23:55 +0800

    Initial commit

---
+++ generate_annual_income_worksheet.py
@@ -0,0 +1,102 @@
+import calendar
+import os
+from datetime import *
+
+import xlsxwriter
+
+"""generate_annual_income_worksheet.py: Automates my personal Income Statement Excel workbook"""
+
+__author__ = "Prajesh Ananthan 2016"
+
+
+def create_excel_sheet(year):
+    workbook = None
+    tablerange = 'B3:D7'
+    filename = getfilename()
+    success = False
+    data = get_itemlist_with_values()
+    options = {'data': data,
+               'columns':
+                   [
+                       {'header': 'ITEM'},
+                       {'header': 'COST'},
+                       {'header': 'STATUS'}
+                   ]
+               }
+
+    try:
+        workbook = xlsxwriter.Workbook('output/{}'.format(filename))
+        monthlist = getListofMonths(year)
+
+        for month in monthlist:
+            worksheet = workbook.add_worksheet(name=month)
+            worksheet.set_column(1, 3, 15)
+            worksheet.add_table(tablerange, options)
+        success = True
+        print("INFO: {} created!".format(workbook.filename))
+
+    except Exception as e:
+        print(e, "ERROR: Unable to write onto {}!".format(filename))
+    finally:
+        workbook.close()
+
+    return [success, workbook.filename]
+
+
+def launchFileInWindows(unixpath):
+    win32path = os.path.normcase(unixpath)
+    try:
+        if os.path.exists(win32path):
+            print("INFO: Launching {}...".format(win32path))
+            os.system(win32path)
+    except Exception as e:
+        print(e)
+
+
+def getListofMonths(year):
+    date1 = datetime.strptime("{}-01-01".format(year), "%Y-%m-%d")
+    date2 = datetime.strptime("{}-01-12".format(year + 1), "%Y-%m-%d")
+    months_str = calendar.month_name
+    months = []
+    while date1 < date2:
+        month = date1.month
+        year = date1.year
+        month_str = months_str[month][0:3]
+        months.append("{0}-{1}".format(month_str, str(year)[-2:]))
+        next_month = month + 1 if month != 12 else 1
+        next_year = year + 1 if next_month == 1 else year
+        date1 = date1.replace(month=next_month, year=next_year)
+
+    return months
+
+
+def getfilename():
+    prefix = "ANNUAL_CASHFLOW"
+    year = "2017"
+    extension = ".xlsx"
+
+    return "{}--{}{}".format(prefix, year, extension)
+
+
+# TODO: Insert more items
+def get_itemlist_with_values():
+    data = [
+        ['Car Monthly Installement', 481],
+        ['House Rent', 450],
+        ['PTPTN', 105.42],
+        ['Rapid KL', 500],
+        ['Touch \'N Go', 64],
+        ['Unifi', 200]
+    ]
+    return data
+
+
+def main():
+    year = 2017
+    [success, filename] = create_excel_sheet(year)
+    if success == True:
+        launchFileInWindows(filename)
+
+
+if __name__ == '__main__':
+    main()
