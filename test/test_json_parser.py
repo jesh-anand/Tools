e3ab194fbf880bcc736ce2e71cebe987557da857
Prajesh Ananthan <prajesh.ananthan@outlook.com>
2016-12-26 08:22:19 +0800

    Added functional test for JSON parser

--- test/test_json_parser.py
+++ test/test_json_parser.py
@@ -1,9 +1,20 @@
 """test_json_parser: Unit test to read json content and pass into dictionary"""
+import json
 
 
-def readJsonFile():
-    pass
+def test_parse_json():
+    content = json.load(open("resources/data.json"))
+    print(content['h264']['video'])
+
+
+def test_iterate_over_json():
+    content = json.load(open("resources/data.json"))
+    for i in content:
+        print(i)
+        for k in content[i]:
+            print(k, content[i][k])
 
 
 if __name__ == '__main__':
-    pass
+    test_parse_json()
+    test_iterate_over_json()
