d0310bb0bf0c15bb4a4ad60c92d6f88b7f1b56ab
Prajesh Ananthan <prajesh.ananthan@bertelsmann.de>
2017-01-16 23:08:18 +0100

    Code refactor

--- income_worksheet_creator/generate_annual_income_worksheet.py
+++ income_worksheet_creator/generate_annual_income_worksheet.py
@@ -15,7 +15,7 @@ def create_excel_sheet(year):
     table_position = 'B3:D13'
     merge_range = 'B2:D2'
     column_header_title = 'MONTHLY CASHFLOW'
-    output = 'output/{}'
+    output_path = 'output/{}'
     filename = getfilename()
     success = False
 
@@ -23,7 +23,7 @@ def create_excel_sheet(year):
     options = {'data': item_list, 'columns': header_title}
 
     try:
-        workbook = xlsxwriter.Workbook(output.format(filename))
+        workbook = xlsxwriter.Workbook(output_path.format(filename))
         merge_format = workbook.add_format({
             'bold': 2,
             'border': 2,
